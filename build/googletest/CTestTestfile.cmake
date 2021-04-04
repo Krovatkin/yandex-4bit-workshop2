f1dd29825a09721c76f722a1f1ad1ea01aa334c3
Nikolay Korovaiko <korovaikon@gmail.com>
2021-04-04 17:53:27 +0000

    remove build

--- build/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/workshop2/googletest
-# Build directory: /workspaces/workshop2/build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- build/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/workshop2/googletest/googletest
-# Build directory: /workspaces/workshop2/build/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
